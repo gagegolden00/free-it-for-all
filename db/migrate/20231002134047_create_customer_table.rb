c35728829e74c3be276b0a9f982418f4a851e5f3
gagegolden00 <malcolm.golden0@gmail.com>
2023-10-02 08:57:38 -0500

    setting up customer migration

---
+++ db/migrate/20231002134047_create_customer_table.rb
@@ -0,0 +1,27 @@
+class CreateCustomerTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :point_of_contact do |t|
+      t.string :name
+      t.string :phone_number
+      t.string :email
+
+      t.timestamps
+      t.timestamp :discarded_at
+    end
+
+    create_table :customer_tables do |t|
+      t.string :name, null: false
+      t.string :phone_number
+      t.string :email
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zipcode
+
+      t.timestamps
+      t.timestamp :discarded_at
+
+      t.references :point_of_contact
+    end
+  end
+end
