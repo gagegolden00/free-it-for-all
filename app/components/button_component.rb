3389bf156d8996256f9a7aac169ece73cefbd9ed
gagegolden00 <malcolm.golden0@gmail.com>
2023-10-05 14:12:32 -0500

    Feature able to alter size of button from params

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -1,6 +1,24 @@
+# frozen_string_literal: true
+
 class ButtonComponent < ViewComponent::Base
-  def initialize(text: "Hello World!", path: "#")
+  BUTTON_SIZES = {
+    small: 'p-1',
+    medium: 'p-2',
+    large: 'p-4'
+  }.freeze
+
+  # scheme, size, block (or full width), align_content, tag , type, request (), disabled (bool),
+
+  def initialize(
+    text: 'Hello World!',
+    path: '#',
+    size: BUTTON_SIZES.fetch(:small)
+  )
+    super
     @text = text
     @path = path
+    @size = size
   end
+
+  def with_icon; end
 end
