382542f9b3ad22178f5bf1738f1fdc1d86dc5ecc
gagegolden00 <malcolm.golden0@gmail.com>
2023-10-06 15:18:46 -0500

    Refactor Reduce parameters by offering prebuilt button functionality To reduce the coplexity of rendering in the template

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -26,11 +26,11 @@
 # extra_classes: "some extra classes here"
 # data: {}
 
+## NEW DOCUMENTATION FOR NEW FUNCTIONS ABOVE REFERENCES INITIAL BRANCH 
 
 # This module just predefines schemes and other styles for consistancy.
 # Schemes can be overriden with the extra_classes attribute
 module SchemeDefinitions
-  # Constants
   SIZES = {
     xsmall: 'p-0.25',
     small: 'p-1',
@@ -40,176 +40,178 @@ module SchemeDefinitions
   }
 
   BUTTON_TYPES = {
-    submit: "submit",
-    button: "button",
-    reset: "reset"
+    submit: 'submit',
+    button: 'button',
+    reset: 'reset',
+    button_link: 'button_link',
   }
 
   DISPLAYS = {
-    # not used for now
-    none: "none",
-    block: "block",
-    flex: "flex"
+    none: 'none',
+    block: 'block',
+    flex: 'flex'
   }
 
   TAGS = {
-    a: "a",
-    button: "button"
+    a: 'a',
+    button: 'button'
   }
 
   SHAPES = {
-    rounded: "rounded"
+    rounded: 'rounded'
   }
 
   METHODS = {
-    get: "get",
-    post: "post"
+    get: 'get',
+    post: 'post'
   }
 
   # Pre defined style_schemes
   STYLING_SCHEMES = {
     default: {
-      text_color: "text-slate-400",
-      bg_color: "",
-      border: "border",
-      border_color: "border-slate-400",
+      text_color: 'text-slate-400',
+      bg_color: '',
+      border: 'border',
+      border_color: 'border-slate-400',
       size: SIZES[:small],
-      display: "",
-      align_content: "items-center",
-      disabled: "",
+      display: '',
+      align_content: 'items-center',
+      disabled: '',
       shape: SHAPES[:rounded],
-      hover: "hover:cursor-pointer",
-      extra_classes: ""
+      hover: 'hover:cursor-pointer',
+      extra_classes: ''
     },
 
     primary: {
-      text_color: "text-white",
-      bg_color: "bg-green-500",
-      border: "border",
-      border_color: "border-green-500",
+      text_color: 'text-white',
+      bg_color: 'bg-green-500',
+      border: 'border',
+      border_color: 'border-green-500',
       size: SIZES[:small],
-      display: "",
-      align_content: "items-center",
-      disabled: "",
+      display: '',
+      align_content: 'items-center',
+      disabled: '',
       shape: SHAPES[:rounded],
-      hover: "hover:bg-green-600 hover:border-green-600 hover:cursor-pointer",
-      extra_classes: ""
+      hover: 'hover:bg-green-600 hover:border-green-600 hover:cursor-pointer',
+      extra_classes: ''
     },
 
     secondary: {
-      text_color: "text-gray-800",
-      bg_color: "bg-gray-200",
-      border: "border",
-      border_color: "border-gray-300",
+      text_color: 'text-gray-800',
+      bg_color: 'bg-gray-200',
+      border: 'border',
+      border_color: 'border-gray-300',
       size: SIZES[:small],
-      display: "",
-      align_content: "items-center",
-      disabled: "",
+      display: '',
+      align_content: 'items-center',
+      disabled: '',
       shape: SHAPES[:rounded],
-      hover: "hover:bg-gray-300 hover:border-gray-400 hover:cursor-pointer",
-      extra_classes: ""
+      hover: 'hover:bg-gray-300 hover:border-gray-400 hover:cursor-pointer',
+      extra_classes: ''
     },
 
     danger: {
-      text_color: "text-white",
-      bg_color: "bg-red-500",
-      border: "border",
-      border_color: "border-red-500",
+      text_color: 'text-white',
+      bg_color: 'bg-red-500',
+      border: 'border',
+      border_color: 'border-red-500',
       size: SIZES[:small],
-      display: "",
-      align_content: "items-center",
-      disabled: "",
+      display: '',
+      align_content: 'items-center',
+      disabled: '',
       shape: SHAPES[:rounded],
-      hover: "hover:bg-red-600 hover:border-red-600 hover:cursor-pointer",
-      extra_classes: ""
+      hover: 'hover:bg-red-600 hover:border-red-600 hover:cursor-pointer',
+      extra_classes: ''
     },
 
     link: {
-      text_color: "",
-      bg_color: "",
-      border: "",
-      border_color: "",
+      text_color: '',
+      bg_color: '',
+      border: '',
+      border_color: '',
       size: SIZES[:small],
-      display: "",
-      align_content: "items-center",
-      disabled: "",
-      shape: "",
-      hover: "hover:cursor-pointer",
-      extra_classes: ""
+      display: '',
+      align_content: 'items-center',
+      disabled: '',
+      shape: '',
+      hover: 'hover:cursor-pointer',
+      extra_classes: ''
     }
   }
 
+  FUNCTIONS = {
+    submit_button: {
+      tag: TAGS[:button],
+      type: BUTTON_TYPES[:submit],
+      method: METHODS[:post]
+    },
 
+    link: {
+      tag: TAGS[:a],
+      type: BUTTON_TYPES[:button_link],
+      method: METHODS[:get]
+    }
+  }
 end
 
 class ButtonComponent < ViewComponent::Base
   include SchemeDefinitions
+
   # destination is maybe not a great name for the
-  def initialize(tag: :button,
-                 id: "your-id-here",
-                 type: :submit,
-                 destination: nil,
-                 text: "Your text here",
-                 method: :post,
-                 styling_scheme: :default,
-                 size: :small,
-                 extra_classes: "some extra classes here",
-                 data: {}
-              )
+  def initialize(
+    styling_scheme: :default,
+    function_scheme: :submit_button, # Provide a default symbol here
+    id: 'your-id-here',
+    destination: nil,
+    text: 'Your text here',
+    extra_classes: 'some extra classes here',
+    data: {}
+  )
     super
-    @tag = tag
     @id = id
-    @type = BUTTON_TYPES[type] || BUTTON_TYPES[:submit]
-    @type_composition = build_type
+    @text = text
 
+    @styling_scheme = STYLING_SCHEMES[styling_scheme] || STYLING_SCHEMES[:default]
+    @extra_classes = extra_classes
+    @class_composition = build_class_scheme
 
+    @selected_function_scheme = FUNCTIONS[function_scheme] || FUNCTIONS[:submit_button]
+    build_function_scheme
     @destination = destination
     @destination_composition = build_destination
 
-
-    @text = text
-    @method = METHODS[method] || METHODS[:post]
-    @styling_scheme = STYLING_SCHEMES[styling_scheme] || STYLING_SCHEMES[:default]
-    @styling_scheme[:size] = SIZES[size] || SIZES[@styling_scheme[:size]]
-    @extra_classes = extra_classes
-    @class_composition = build_class_composition
     @data = data
     @data_attributes = build_data_attributes
+
   end
 
   private
 
-  def build_class_composition
-    []
-    tailwind_class_array = @styling_scheme.values.reject(&:blank?).join(" ")
-    tailwind_class_array + " " + @extra_classes
+  def build_class_scheme
+    tailwind_class_array = @styling_scheme.values.reject(&:blank?).join(' ')
+    tailwind_class_array + ' ' + @extra_classes
+  end
+
+  def build_function_scheme
+    @tag = @selected_function_scheme[:tag]
+    @type = "type=#{@selected_function_scheme[:type]}"
+    if @selected_function_scheme[:method] == :post
+      @method = "formmethod=#{@selected_function_scheme[:method]}"
+    elseif @selected_function_scheme[:method] == :get
+      @method = "method=#{@selected_function_scheme[:method]}"
+    end
   end
 
   def build_data_attributes
     return if @data.nil?
-    @data.map { |key, value| "data-#{key.to_s.gsub('_', '-')}=#{value.to_s.gsub("_", "-")}" }.join(" ")
+    @data.map { |key, value| "data-#{key.to_s.gsub('_', '-')}=#{value.to_s.gsub('_', '-')}" }.join(' ')
   end
 
   def build_destination
-    puts @destination
-    case
-    when @tag == :a && !@destination.nil?
+    if @selected_function_scheme[:tag] == "a" && !@destination.nil?
       "href=#{@destination}"
-    when @tag == :button && !@destination.nil?
+    elsif @selected_function_scheme[:tag] == "button" && !@destination.nil?
       "url=#{@destination}"
-    else
-      nil
     end
   end
-
-
-  def build_type
-    case @type
-    when nil
-      nil
-    else
-      "type=#{@type}"
-    end
-  end
-
 end
