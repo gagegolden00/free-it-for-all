2b537c645060f5b660badfdd827614c39010129a
gagegolden00 <malcolm.golden0@gmail.com>
2023-10-10 14:45:17 -0500

    Refactor

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -8,7 +8,7 @@
 # id:              | string      | Text to set the id of the element
 # href:            | string      | Text to specify the path if the elemet is a link
 # text:            | string      | Text to appear inside the button or link
-# extra_classes:   | string      | Extra classes that will override the default styling scheme
+# custom_cl:   | string      | Extra classes that will override the default styling scheme
 # data:            | hash/symbols| Data attributes for javascript & stimulus
 # leading_visual:  | hash/symbols| Attributes for the class and style of the leading visual
 
@@ -62,7 +62,7 @@ module SchemeDefinitions
       disabled: nil,
       shape: SHAPES[:rounded],
       hover: 'hover:cursor-pointer hover:bg-slate-400 hover:text-white',
-      extra_classes: nil
+      custom_class: nil
     },
 
     primary: {
@@ -77,7 +77,7 @@ module SchemeDefinitions
       disabled: nil,
       shape: SHAPES[:rounded],
       hover: 'hover:bg-green-600 hover:border-green-600 hover:cursor-pointer',
-      extra_classes: nil
+      custom_class: nil
     },
 
     secondary: {
@@ -92,7 +92,7 @@ module SchemeDefinitions
       disabled: nil,
       shape: SHAPES[:rounded],
       hover: 'hover:bg-gray-300 hover:border-gray-400 hover:cursor-pointer',
-      extra_classes: nil
+      custom_class: nil
     },
 
     danger: {
@@ -107,7 +107,7 @@ module SchemeDefinitions
       disabled: nil,
       shape: SHAPES[:rounded],
       hover: 'hover:bg-red-600 hover:border-red-600 hover:cursor-pointer',
-      extra_classes: nil
+      custom_class: nil
     },
 
     link: {
@@ -122,7 +122,7 @@ module SchemeDefinitions
       disabled: nil,
       shape: nil,
       hover: 'hover:cursor-pointer',
-      extra_classes: nil
+      custom_class: nil
     }
   }
 
@@ -154,7 +154,7 @@ class ButtonComponent < ViewComponent::Base
     id: '',
     href: '',
     text: '',
-    extra_classes: '',
+    custom_class: '',
     # sanatize data attributes?
     data: {},
     leading_visual: {},
@@ -166,7 +166,7 @@ class ButtonComponent < ViewComponent::Base
     @id = id
     @href = href
     @text = text
-    @extra_classes = extra_classes
+    @custom_class = custom_class
     @data = data
     @leading_visual = leading_visual
     @trailing_visual = trailing_visual
@@ -195,8 +195,12 @@ class ButtonComponent < ViewComponent::Base
   end
 
   def build_html_element_style_scheme
-    tailwind_class_array = @styling_scheme.values.reject(&:blank?)
-    @class_composition = tailwind_class_array.join(' ') + ' ' + @extra_classes.to_s
+    if @custom_class.empty?
+      tailwind_class_array = @styling_scheme.values.reject(&:blank?)
+      @class_composition = tailwind_class_array.join(' ')
+    else
+      @class_composition = @custom_class
+    end
   end
 
   def build_html_element_data_attributes
@@ -209,12 +213,12 @@ class ButtonComponent < ViewComponent::Base
   def build_html_element_leading_visual
     return if @leading_visual.nil? || @leading_visual.empty?
     @leading_visual_class_attribute_value = @leading_visual[:class] ? "#{@leading_visual[:class]}" : ""
-    @leading_visual_style_attribute_value = @leading_visual[:style] ? "#{@leading_visual[:style]}" + " margin-right: auto;": ""
+    @leading_visual_style_attribute_value = @leading_visual[:style] ? "#{@leading_visual[:style]}" + "padding-right: 5px;": ""
   end
 
   def build_html_element_trailing_visual
     return if @trailing_visual.nil? || @trailing_visual.empty?
     @trailing_visual_class_attribute_value = @trailing_visual[:class] ? "#{@trailing_visual[:class]}": ""
-    @trailing_visual_style_attribute_value = @trailing_visual[:style] ? "#{@trailing_visual[:style]}" + " margin-left: auto;": ""
+    @trailing_visual_style_attribute_value = @trailing_visual[:style] ? "#{@trailing_visual[:style]}" + "padding-left: 5px;": ""
   end
 end
