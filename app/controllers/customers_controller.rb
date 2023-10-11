6bcb0106a2ca2c2a6c95e3f8434e7bedc1d21eac
gagegolden00 <malcolm.golden0@gmail.com>
2023-10-10 19:51:24 -0500

    Added in the customers CRUD

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,60 @@
+class CustomersController < ApplicationController
+  before_action :set_customer_from_params, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @customer = Customer.new
+    @regions = Region.all
+    @customer.build_point_of_contact
+  end
+
+  def create
+    @regions = Region.all
+    @customer = Customer.new(customer_params)
+    if @customer.save!
+      flash[:notice] = "Customer created"
+      redirect_to @customer
+    else
+      render :new
+    end
+  end
+
+  def show;end
+
+  def index
+    @customers = Customer.all
+  end
+
+  def edit
+    @regions = Region.all
+    @customer.build_point_of_contact
+  end
+
+  def update
+    if @customer.update!(customer_params)
+      flash[:notice] = "Customer updated"
+      redirect_to @customer
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @customer.discard
+      flash[:notice] = "Customer removed"
+      redirect_to customers_path
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :name, :phone_number, :email, :address, :city, :state, :zipcode, :region_id,
+      point_of_contact_attributes: [:name, :phone_number, :email])
+  end
+
+  def set_customer_from_params
+    @customer = Customer.find(params[:id])
+  end
+
+end
