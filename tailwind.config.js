0b06adfe71809b62b9676bb32432aded0cd831e0
Jason Zahn <jason@rubyshore.com>
2023-10-16 12:05:51 -0500

    flowbite install and test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 module.exports = {
+  plugins: [
+    require('flowbite/plugin')
+  ],
+
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
-    './app/components/**/*.html.erb'
-  ]
+    './app/components/**/*.html.erb',
+    './node_modules/flowbite/**/*.js'
+  ],
+
+  theme: {
+    colors: {
+      'primary': '#3673B7'
+    }
+  }
 }
