e9a486403947d0c9723f61aadd7c949954238b2a
gagegolden00 <malcolm.golden0@gmail.com>
2023-10-17 09:23:28 -0500

    Refactor into sub classees

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -1,32 +1,16 @@
 class ButtonComponent < ViewComponent::Base
-
-  def initialize(tag: '', type: '', id: '', href: '', text: '', tailwind_class: '', data: {}, leading_visual: {}, trailing_visual: {})
-    @tag = tag
-    @type = type
-    @id = id
-    @href = href
-    @text = text
-    @tailwind_class = tailwind_class
-    @data = build_html_element_data_attributes || nil
-    @leading_visual = build_html_element_leading_visual || nil
-    @trailing_visual = build_html_element_trailing_visual || nil
-  end
-
   def build_html_element_data_attributes
-    return if @data.nil?
     @data_attributes = @data.map do |key, value|
       "data-#{key.to_s.gsub('_', '-')}=#{value.to_s.gsub('_', '-')}"
     end.join(' ')
   end
 
   def build_html_element_leading_visual
-    return if @leading_visual.nil? || @leading_visual.empty?
     @leading_visual_class_attribute_value = @leading_visual[:class] ? "#{@leading_visual[:class]}" : ''
     @leading_visual_style_attribute_value = @leading_visual[:style] ? "#{@leading_visual[:style]}" + 'padding-right: 5px;' : ''
   end
 
   def build_html_element_trailing_visual
-    return if @trailing_visual.nil? || @trailing_visual.empty?
     @trailing_visual_class_attribute_value = @trailing_visual[:class] ? "#{@trailing_visual[:class]}" : ''
     @trailing_visual_style_attribute_value = @trailing_visual[:style] ? "#{@trailing_visual[:style]}" + 'padding-left: 5px;' : ''
   end
@@ -34,33 +18,51 @@ end
 
 class ButtonComponent::Submit < ButtonComponent
   def initialize(
-    tag: 'button',
-    type: 'submit',
-    text: 'hey im a button',
+    id: '',
+    text: 'Hey im a button',
     tailwind_class: 'text-green-400 border border-green-400 p-1 h-8 w-auto inline-flex items-center text-center rounded hover:cursor-pointer hover:bg-green-400 hover:text-white',
     data: {},
     leading_visual: {},
     trailing_visual: {}
     )
-    @tag = tag
-    @type = type
+    @id = id
     @text = text
     @tailwind_class = tailwind_class
     @data = data
     @leading_visual = leading_visual
     @trailing_visual = trailing_visual
+    build_html_element_data_attributes unless @data.nil? || @data.empty?
+    build_html_element_leading_visual unless @leading_visual.nil? || @leading_visual.empty?
+    build_html_element_trailing_visual unless @trailing_visual.nil? || @trailing_visual.empty?
   end
 end
 
 class ButtonComponent::Button < ButtonComponent
-  def initialize
-    super
+  def initialize(id: '', text: '', tailwind_class: '', data: {}, leading_visual: {}, trailing_visual: {})
+    @id = id
+    @text = text
+    @tailwind_class = tailwind_class
+    @data = data
+    @leading_visual = leading_visual
+    @trailing_visual = trailing_visual
+    build_html_element_data_attributes unless @data.nil? || @data.empty?
+    build_html_element_leading_visual unless @leading_visual.nil? || @leading_visual.empty?
+    build_html_element_trailing_visual unless @trailing_visual.nil? || @trailing_visual.empty?
   end
 end
 
 class ButtonComponent::Link < ButtonComponent
-  def initialize
-    super
+  def initialize(id: '', href: '', text: '', tailwind_class: '', data: {}, leading_visual: {}, trailing_visual: {})
+    @id = id
+    @href = href
+    @text = text
+    @tailwind_class = tailwind_class
+    @data = data
+    @leading_visual = leading_visual
+    @trailing_visual = trailing_visual
+    build_html_element_data_attributes unless @data.nil? || @data.empty?
+    build_html_element_leading_visual unless @leading_visual.nil? || @leading_visual.empty?
+    build_html_element_trailing_visual unless @trailing_visual.nil? || @trailing_visual.empty?
   end
 end
 
