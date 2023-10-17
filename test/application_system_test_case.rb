4d2d12c8c0c06579248f28931b7e1faf6eb2a066
gagegolden00 <malcolm.golden0@gmail.com>
2023-10-17 11:49:33 -0500

    Fix
    reverted the app system test case inherientence

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,5 @@
 require "test_helper"
 
-class ApplicationSystemTestCase < ActiveSupport::TestCase
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
