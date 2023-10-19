21b509ced0ee33f434c3bcaa605d33eab7f6d505
gagegolden00 <malcolm.golden0@gmail.com>
2023-10-19 08:13:24 -0500

    Fix customer crud
    - Rename routes based off of upcoming changes in service jobs PR
    - Add validations to POC
    - Add blank name check in customer model for POC
    - Update customer form to include path variable passed by the partial render in the new & edit pages

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,11 @@
 class Customer < ApplicationRecord
-  has_one :point_of_contact, dependent: :destroy
-  belongs_to :region
-  accepts_nested_attributes_for :point_of_contact
+  has_one :point_of_contact, dependent: :destroy, required: false
+  belongs_to :region, required: false
+  has_many :service_jobs
+
+  validates :name, presence: true
+
+  accepts_nested_attributes_for :point_of_contact, reject_if: :name_blank?
 
   before_discard :discard_necessary_associated_records
 
@@ -11,5 +15,9 @@ class Customer < ApplicationRecord
 
   def discard_necessary_associated_records
     point_of_contact.discard if point_of_contact.present?
+  end
+
+  def name_blank?(attributes)
+    attributes['name'].blank?
   end
 end
