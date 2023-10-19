a928356e5d212c36291a3322365aa2beba88bfa2
gagegolden00 <malcolm.golden0@gmail.com>
2023-10-19 15:28:02 -0500

    Refactor stimulus controllers
    - Just split the functionality of one controller action into two

--- app/javascript/controllers/toggle_hidden_controller.js
+++ app/javascript/controllers/toggle_hidden_controller.js
@@ -5,16 +5,26 @@ export default class extends Controller {
     console.log("Connected to toggle-hidden controller")
   }
 
-  toggle(event) {
+  toggleVisibility(event) {
     const targetIds = event.currentTarget.dataset.targetId.split(' ');
     targetIds.forEach(id => {
       const element = document.getElementById(id);
       if (element.classList.contains("hidden")) {
         element.classList.remove("hidden");
-        element.removeAttribute("disabled");
       } else {
         element.classList.add("hidden");
-        element.setAttribute("disabled", "true");
+      }
+    });
+  }
+
+  toggleDisabled(event) {
+    const targetIds = event.currentTarget.dataset.targetId.split(' ');
+    targetIds.forEach(id => {
+      const element = document.getElementById(id);
+      if (element.disabled) {
+        element.disabled = false;
+      } else {
+        element.disabled = true;
       }
     });
   }
