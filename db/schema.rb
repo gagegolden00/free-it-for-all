b13759c01f02e8995864fb901c52c2a3c4dd4c9b
gagegolden00 <malcolm.golden0@gmail.com>
2023-10-21 10:20:05 -0500

    Feature add darkmode button
    - add the button with svgs
    - add the stimulus controller to handle toggling darkmode,
    -  setting the local storage variable, retrieving the local storage variables
    -  then applying dark more based off of memory

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_132739) do
 
   # Custom types defined in this database.
   # Note that some types may not work with other database engines. Be careful if changing database.
-  create_enum "roles", ["admin", "technician"]
+  create_enum "role", ["admin", "technician"]
   create_enum "status", ["Pending", "Not started", "In progress", "On hold", "Completed"]
 
   create_table "customers", force: :cascade do |t|
@@ -30,32 +30,32 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_132739) do
     t.string "address"
     t.string "city"
     t.string "state"
-    t.integer "zipcode"
+    t.integer "zip_code"
     t.bigint "region_id"
-    t.timestamptz "created_at", precision: 6, null: false
-    t.timestamptz "updated_at", precision: 6, null: false
+    t.timestamptz "created_at", null: false
+    t.timestamptz "updated_at", null: false
     t.timestamptz "discarded_at"
     t.index ["discarded_at"], name: "index_customers_on_discarded_at"
     t.index ["region_id"], name: "index_customers_on_region_id"
   end
 
   create_table "point_of_contacts", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.string "phone_number"
     t.string "email"
     t.bigint "customer_id"
-    t.timestamptz "created_at", precision: 6, null: false
-    t.timestamptz "updated_at", precision: 6, null: false
+    t.timestamptz "created_at", null: false
+    t.timestamptz "updated_at", null: false
     t.timestamptz "discarded_at"
     t.index ["customer_id"], name: "index_point_of_contacts_on_customer_id"
     t.index ["discarded_at"], name: "index_point_of_contacts_on_discarded_at"
   end
 
   create_table "regions", force: :cascade do |t|
-    t.string "name"
-    t.string "manager"
-    t.timestamptz "created_at", precision: 6, null: false
-    t.timestamptz "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.string "manager", null: false
+    t.timestamptz "created_at", null: false
+    t.timestamptz "updated_at", null: false
     t.timestamptz "discarded_at"
     t.index ["discarded_at"], name: "index_regions_on_discarded_at"
   end
@@ -68,8 +68,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_132739) do
     t.string "work_type"
     t.bigint "customer_id"
     t.bigint "work_site_id"
-    t.timestamptz "created_at", precision: 6, null: false
-    t.timestamptz "updated_at", precision: 6, null: false
+    t.timestamptz "created_at", null: false
+    t.timestamptz "updated_at", null: false
     t.timestamptz "discarded_at"
     t.index ["customer_id"], name: "index_service_jobs_on_customer_id"
     t.index ["discarded_at"], name: "index_service_jobs_on_discarded_at"
@@ -77,20 +77,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_132739) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.enum "role", null: false, enum_type: "roles"
+    t.enum "role", null: false, enum_type: "role"
     t.string "name", null: false
     t.string "home_phone"
     t.string "work_phone"
     t.string "carrier"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
     t.date "hire_date"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.timestamptz "reset_password_sent_at", precision: 6
-    t.timestamptz "remember_created_at", precision: 6
-    t.timestamptz "created_at", precision: 6, null: false
-    t.timestamptz "updated_at", precision: 6, null: false
+    t.timestamptz "reset_password_sent_at"
+    t.timestamptz "remember_created_at"
+    t.timestamptz "created_at", null: false
+    t.timestamptz "updated_at", null: false
     t.timestamptz "discarded_at"
+    t.index ["discarded_at"], name: "index_users_on_discarded_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -103,13 +108,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_12_132739) do
     t.integer "zip_code"
     t.string "email"
     t.string "phone_number"
-    t.timestamptz "created_at", precision: 6, null: false
-    t.timestamptz "updated_at", precision: 6, null: false
+    t.timestamptz "created_at", null: false
+    t.timestamptz "updated_at", null: false
     t.timestamptz "discarded_at"
     t.index ["discarded_at"], name: "index_work_sites_on_discarded_at"
   end
 
-  add_foreign_key "customers", "regions"
-  add_foreign_key "point_of_contacts", "customers"
   add_foreign_key "service_jobs", "customers"
 end
