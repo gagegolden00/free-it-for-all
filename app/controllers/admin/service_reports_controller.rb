35e8f8ef5aa3dfdb673c091a183313b4997ad36a
gagegolden00 <malcolm.golden0@gmail.com>
2023-10-30 07:53:02 -0500

    Refactor code Fix small issues

--- app/controllers/admin/service_reports_controller.rb
+++ app/controllers/admin/service_reports_controller.rb
@@ -1,42 +1,84 @@
 class Admin::ServiceReportsController < ApplicationController
+  before_action :set_service_job, only: %i[new create show index edit update destroy]
+  before_action :set_service_report, only: %i[show edit update destroy]
+  before_action :set_materials, only: %i[new create edit update]
+  before_action :set_existing_service_report_materials, only: %i[edit update]
 
   def new
     @service_report = ServiceReport.new
-    @materials = Material.kept
   end
 
   def create
-    filtered_params = service_report_params
-    filtered_params[:service_report_materials_attributes] = filtered_params[:service_report_materials_attributes].select do |_, attributes|
-      attributes[:quantity].to_i > 0
+    filtered_params = filter_params(service_report_params)
+    @service_report = ServiceReport.create!(filtered_params)
+    if @service_report.save
+      flash[:notice] = "Service report created"
+      redirect_to admin_service_job_service_report_path(@service_job, @service_report)
+    else
+      render :new
     end
-    @service_report = ServiceReport.create(filtered_params)
   end
 
   def show
-
+    @service_report_materials = ServiceReport.materials_used(@service_report)
   end
 
   def index
-
+    @service_reports = @service_job.service_reports.kept
   end
 
   def edit
-
+    @service_report.service_report_materials.build
   end
 
   def update
-
+    filtered_params = filter_params(service_report_params)
+    if @service_report.update(filtered_params)
+      flash[:notice] = "Service report updated"
+      redirect_to admin_service_job_service_report_path(@service_job, @service_report)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @service_report.discard
+      flash[:notice] = "Report deleted"
+      redirect_to admin_service_job_service_reports_path(@service_job)
+    end
 
   end
 
   private
 
-  def set_service_report_from_params
-    @service_report = ServiceReport.find(params[:id])
+  def set_service_job
+    @service_job = ServiceJob.find(params[:service_job_id])
+  end
+
+  def set_service_report
+    @service_report = @service_job.service_reports.find(params[:id])
+  end
+
+  def set_materials
+    @materials = Material.kept.includes(:service_report_materials)
+  end
+
+  def set_existing_service_report_materials
+    @existing_materials_by_id = @service_report.service_report_materials.index_by(&:material_id) if @service_report
+  end
+
+
+  def filter_params(params)
+    params[:service_report_materials_attributes] = params[:service_report_materials_attributes].select do |_, service_report_material_object|
+      binding.pry
+      quantity = service_report_material_object[:quantity].to_i
+      if service_report_material_object[:id].present?
+        true
+      else
+        quantity.positive?
+      end
+    end
+    params
   end
 
   def service_report_params
@@ -51,7 +93,8 @@ class Admin::ServiceReportsController < ApplicationController
       :customer_signature,
       :description,
       :service_job_id,
-      service_report_materials_attributes: [:material_id, :quantity, :_destroy])
+      service_report_materials_attributes: %i[id material_id quantity _destroy]
+    )
   end
-
 end
+
--- app/models/service_report.rb
+++ app/models/service_report.rb
@@ -5,12 +5,19 @@ class ServiceReport < ApplicationRecord
   accepts_nested_attributes_for :service_report_materials
 
   validates :service_job_id, presence: true
+  # validates :service_report_number, presence: true
+  validates :equipment_model, presence: true
+  validates :equipment_serial, presence: true
+  validates :description, presence: true
+
+
+  scope :materials_used, ->(service_report) {
+    joins(service_report_materials: :material)
+      .where(id: service_report.id)
+      .where('service_report_materials.quantity > 0')
+      .select('materials.name, materials.price, service_report_materials.quantity, service_report_materials.pre_tax_total')
+  }
 
   private
-
-  def quantity_blank?(attributes)
-    result = attributes['quanity'].blank?
-    result
-  end
 
 end
