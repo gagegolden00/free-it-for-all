29566f6ef393d2973a385ba70bf0dceb18fa4aa2
gagegolden00 <malcolm.golden0@gmail.com>
2023-11-17 08:57:27 -0600

    Add the important to module export tailwind config Fix price format helper Add callback for calculating service report material prices Add styling for print/export service report

--- app/helpers/view_price_format_helper.rb
+++ app/helpers/view_price_format_helper.rb
@@ -1,7 +1,11 @@
 module ViewPriceFormatHelper
   def format_price(price_in_cents)
-    dollars = price_in_cents / 100
-    cents = price_in_cents % 100
+    if !price_in_cents.nil?
+    dollars = price_in_cents / 100 if price_in_cents
+    cents = price_in_cents % 100 if price_in_cents
     sprintf('$%d.%02d', dollars, cents)
+    else
+      0
+    end
   end
 end
