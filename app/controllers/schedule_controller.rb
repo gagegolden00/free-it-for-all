1d565266236f30fcf53865e9d702d9624b731491
gagegolden00 <malcolm.golden0@gmail.com>
2023-12-20 16:33:43 -0600

    Commiting to fetch latest changes

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -2,5 +2,12 @@ class ScheduleController < ApplicationController
     layout 'application_full'
 
     def index
+    # start with 1 user
+
+    @user_service_job = UserServiceJob.joins(:users).first
+
+    
+
     end
+
 end
