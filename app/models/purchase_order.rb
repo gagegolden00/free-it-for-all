3405702b02b80f9033bd884c27e5a34a0f0f2385
gagegolden00 <malcolm.golden0@gmail.com>
2024-01-04 08:27:26 -0600

    Add model and front end validations to purchase orders

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -2,6 +2,10 @@ class PurchaseOrder < ApplicationRecord
   belongs_to :service_report
   before_save :calculate_total_price
 
+  validates :purchase_order_number, presence: true
+  validates :description, presence: true
+  validates :service_report_id, presence: true
+
   def calculate_total_price
     self.total_price = price * quantity
   end
