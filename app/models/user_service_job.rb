90138cd2f5377a9e134dc875094ad77f309bcab4
gagegolden00 <malcolm.golden0@gmail.com>
2024-01-04 10:32:24 -0600

    Add session to get error messages from user service jobs controller to schedule controller
    Copy the alert component styling to the schedule index to use the
    array of errors in the schedule page
    
    add validations and custom validation to make sure the time
    inputs are proper.
    
    The alert component expects an object but since I am using a
    session to store the messages in an array I could not use the
    component itself but could use the styling.

--- app/models/user_service_job.rb
+++ app/models/user_service_job.rb
@@ -2,10 +2,23 @@ class UserServiceJob < ApplicationRecord
   belongs_to :service_job
   belongs_to :user
 
-  scope :active_users_for_service_job, ->(service_job_id) do
+  validates :date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validate :start_time_before_end_time
+
+  scope :active_users_for_service_job, lambda { |service_job_id|
     joins(:user)
       .select('users.*, user_service_jobs.id AS user_service_job_id, user_service_jobs.*')
-      .where(discarded_at: nil, service_job_id: service_job_id)
-  end
+      .where(discarded_at: nil, service_job_id:)
+  }
 
+  private
+
+  def start_time_before_end_time
+    puts start_time
+    puts end_time
+    return if start_time && end_time && !start_time.after?(end_time)
+    errors.add(:base, 'The start time must be before the end time for any given day')
+  end
 end
