48720682d6b49a9609a38d419e2a10b95769f01e
gagegolden00 <malcolm.golden0@gmail.com>
2024-01-29 14:11:53 -0600

    Finish auth

--- app/policies/ipv4_quiz_policy.rb
+++ app/policies/ipv4_quiz_policy.rb
@@ -1,8 +1,42 @@
 class Ipv4QuizPolicy < ApplicationPolicy
+  def initialize(user, quiz)
+    @user = user
+    @quiz = quiz
+  end
+
+  def index?
+    @user.present?
+  end
+
+  def show?
+    @user.present?
+  end
+
+  def create?
+    @user.present?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    @user.present?
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
   end
 end
+
